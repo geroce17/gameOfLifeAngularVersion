b53394b25bf5b6eecf7c73604a16c73c56897113
geroce17 <geroce@outlook.com>
2017-10-16 22:37:40 -0500

    Version 0.2 ngFor

--- src/app/Components/my-component.ts
+++ src/app/Components/my-component.ts
@@ -10,26 +10,15 @@ export class MyComponent {
     Arr1: number [] = new Array();
     num = null;
 
-    generarTabla(size: number) {
-        document.getElementById('tableroSection').innerHTML = '';
-        // document.getElementById('tableroSection').className = 'table-responsive';
-        const body = document.getElementsByTagName('section')[0];
-        const tabla = document.createElement('table');
-        for (let i = 0; i < size; i++) {
-            // matriz[i]=new Array(3);
-            const hilera = document.createElement('tr');
-            for (let j = 0; j < size; j++) {
-                const celda = document.createElement('td');
-                celda.addEventListener('click', (e: Event) => this.changeColor(celda));
-                celda.style.backgroundColor = 'black';
-                celda.style.padding = '10px';
-                // matriz[i][j]=celda;
-                hilera.appendChild(celda);
-            }
-            tabla.appendChild(hilera); // agrega la hilera al final de la tabla (al final del elemento tblbody)
+    getSize() {
+        this.num = parseFloat((<HTMLInputElement>document.getElementById('size')).value);
+        for (let index = 0; index < this.num; index++) {
+            this.Arr1.push(index);
         }
-        body.appendChild(tabla);
-        tabla.setAttribute('border', '.1');
+    }
+
+    clean() {
+        this.Arr1 = [];
     }
 
     changeColor(celda: HTMLTableCellElement) {
@@ -52,6 +41,30 @@ export class MyComponent {
         for (let index = 0; index < this.num; index++) {
             this.Arr1.push(index);
         }
+    }
+     */
+
+     /*
+     generarTabla(size: number) {
+        document.getElementById('tableroSection').innerHTML = '';
+        // document.getElementById('tableroSection').className = 'table-responsive';
+        const body = document.getElementsByTagName('section')[0];
+        const tabla = document.createElement('table');
+        for (let i = 0; i < size; i++) {
+            // matriz[i]=new Array(3);
+            const hilera = document.createElement('tr');
+            for (let j = 0; j < size; j++) {
+                const celda = document.createElement('td');
+                celda.addEventListener('click', (e: Event) => this.changeColor(celda));
+                celda.style.backgroundColor = 'black';
+                celda.style.padding = '10px';
+                // matriz[i][j]=celda;
+                hilera.appendChild(celda);
+            }
+            tabla.appendChild(hilera); // agrega la hilera al final de la tabla (al final del elemento tblbody)
+        }
+        body.appendChild(tabla);
+        tabla.setAttribute('border', '.1');
     }
      */
 }
